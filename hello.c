1cd7345d21a60af079a959c0d6edb2e7c6d2cf4f
Kevin Mullins <williepete230@outlook.com>
2023-08-14 13:48:58 -0400

    added major to the output

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * @file hello.c
+ * @author Kevin Mullins williepete230@outlook.com
+ * @brief prints computer science as major
+ * @version 0.1
+ * @date 2023-08-14
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Major is Computer Science");
+
+    return 0;
+}
